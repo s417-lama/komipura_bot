0b1cf3d30d06ab51f5338a9ae59477312eb01093
slama <s417.lama@gmail.com>
2018-07-28 22:15:43 +0900

    append url at the last of the tweet

--- lib/komipura_bot.ex
+++ lib/komipura_bot.ex
@@ -5,7 +5,7 @@ defmodule KomipuraBot do
 
   @url "http://www.com-pla.com/reservation/index.php"
 
-  def post(url, month, year) do
+  defp post(url, month, year) do
     body = "target_ym%5By%5D=#{year}&target_ym%5Bm%5D=#{month}&house=north&facility_id=&usehour_id=&act=&val="
     header = [
       {"Content-Type", "application/x-www-form-urlencoded"},
@@ -14,14 +14,14 @@ defmodule KomipuraBot do
     HTTPoison.post!(url, body, header)
   end
 
-  def fetch(day, month, year) do
+  defp fetch(day, month, year) do
     post(@url, month, year)
     |> handle_result()
     |> day_info(day)
     |> holiday_filter(day, month, year)
   end
 
-  def holiday_filter(data, day, month, year) do
+  defp holiday_filter(data, day, month, year) do
     date = Date.from_erl!({year, month, day})
     case Date.day_of_week(date) do
       weekday when weekday < 6 ->
@@ -29,79 +29,81 @@ defmodule KomipuraBot do
           true  -> remove_1period(data)
           false -> data
         end
-      _ ->
-        remove_1period(data)
+      _ -> remove_1period(data)
     end
   end
 
-  def remove_1period(data) do
+  defp remove_1period(data) do
     Enum.map(data, fn([_head | tail]) -> ["-" | tail] end)
   end
 
-  def handle_result(%HTTPoison.Response{body: html, status_code: 200}) do
-    html
-  end
+  defp handle_result(%HTTPoison.Response{body: html, status_code: 200}), do: html
 
-  def day_info(html, day) do
+  defp day_info(html, day) do
     html
     |> Floki.find("table table")
     |> Enum.take(-3)
     |> Enum.map(fn h -> (1..7 |> Enum.map(&(period_info(h, day, &1)))) end)
   end
 
-  def period_info(html, day, period) do
+  defp period_info(html, day, period) do
     html
     |> Floki.find("tr:nth-child(#{period + 2}) th:nth-child(#{day + 1})")
     |> Floki.find("font")
-    |> Floki.text
-    |> is_ok
+    |> Floki.text()
+    |> is_ok()
   end
 
-  def is_ok(<<129, 155>>), do: "○"
-  def is_ok(<<129, 126>>), do: "×"
+  defp is_ok(<<129, 155>>), do: "○"
+  defp is_ok(<<129, 126>>), do: "×"
 
-  def tweet(text) do
+  defp tweet(text) do
     ExTwitter.update(text)
   end
 
-  def tweet_format_row(n, data) do
+  defp tweet_format_row(n, data) do
     data
     |> Enum.map(&(Enum.at(&1, n)))
     |> Enum.join("     ")
     |> row_title(n)
   end
 
-  def row_title(text, n) when n < 2, do: "#{n + 1}限     " <> text
-  def row_title(text, 2)           , do: "昼        "      <> text
-  def row_title(text, n) when n > 2, do: "#{n}限     "     <> text
+  defp row_title(text, n) when n < 2, do: "#{n + 1}限     " <> text
+  defp row_title(text, 2)           , do: "昼        "      <> text
+  defp row_title(text, n) when n > 2, do: "#{n}限     "     <> text
 
-  def set_title(text, day, month) do
+  defp set_title(text, day, month) do
     case :calendar.local_time() do
       {{_year, ^month, ^day}, _} -> "本日のコミプラ予約状況\n"           <> "  身体1 身体2 身体3\n" <> text
       _                          -> "#{month}/#{day} コミプラ予約状況\n" <> "  身体1 身体2 身体3\n" <> text
     end
   end
 
-  def tweet_format(data, day, month) do
+  defp tweet_format(data, day, month) do
     0..6
     |> Enum.map(fn n -> tweet_format_row(n, data) end)
     |> Enum.join("\n")
     |> set_title(day, month)
+    |> append_url()
   end
 
-  def fetch_and_tweet(day, month, year) do
+  defp append_url(text) do
+    text <> "\n\n" <> @url
+  end
+
+  defp fetch_and_tweet(day, month, year) do
     fetch(day, month, year)
     |> tweet_format(day, month)
     |> tweet()
   end
 
   def exec() do
-    {{year, month, day}, _} = :calendar.local_time
+    {{year, month, day}, _} = :calendar.local_time()
     fetch_and_tweet(day, month, year)
   end
 
   def exec_tomorrow() do
-    {{year, month, day}, _} = :calendar.local_time |> Timex.shift(days: 1)
+    {{year, month, day}, _} = :calendar.local_time() |> Timex.shift(days: 1)
     fetch_and_tweet(day, month, year)
   end
 end
